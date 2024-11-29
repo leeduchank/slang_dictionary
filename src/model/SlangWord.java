36ca11844c329996a863152f69740a8085fc9c68
DucHanh Le <leDucHanh>
2024-11-29 21:33:19 +0700

    create menu and add SlangWord struct

--- src/model/SlangWord.java
+++ src/model/SlangWord.java
@@ -1,2 +1,48 @@
-package model;public class SlangWord {
+package model;
+
+import java.util.Objects;
+
+
+public class SlangWord {
+    private String word;
+    private String definition;
+
+    public SlangWord(String word, String definition) {
+        this.word = word;
+        this.definition = definition;
+    }
+
+    public String getWord() {
+        return word;
+    }
+
+    public void setWord(String word) {
+        this.word = word;
+    }
+
+    public String getDefinition() {
+        return definition;
+    }
+
+    public void setDefinition(String definition) {
+        this.definition = definition;
+    }
+
+    @Override
+    public String toString() {
+        return word + ": " + definition;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+        SlangWord that = (SlangWord) obj;
+        return Objects.equals(word, that.word);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(word);
+    }
 }
